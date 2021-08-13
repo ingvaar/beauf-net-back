aa3a5f2b8c345720a65722ce9c18365b86f5f7a3
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-08-13 19:14:51 +0200

    feature/deployment -> Hotfix API after deployment

--- src/user/user.profile.ts
+++ src/user/user.profile.ts
@@ -3,8 +3,8 @@
 import { AutomapperProfile, InjectMapper } from '@automapper/nestjs';
 import { Mapper, MappingProfile } from '@automapper/types';
 import { Injectable } from '@nestjs/common';
-import { UserPublicDto } from 'src/user/dto/user.public.dto';
-import { UserEntity } from 'src/user/user.entity';
+import { UserPublicDto } from '../user/dto/user.public.dto';
+import { UserEntity } from '../user/user.entity';
 
 @Injectable()
 export class UserProfile extends AutomapperProfile {
