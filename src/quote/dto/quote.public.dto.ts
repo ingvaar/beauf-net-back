c94ab03b3c2cfdaa3eec168901c166de45224422
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-08-14 14:14:55 +0200

    feature/quotes -> mapping + addQuote

--- src/quote/dto/quote.public.dto.ts
+++ src/quote/dto/quote.public.dto.ts
@@ -1,9 +1,15 @@
+import { AutoMap } from "@automapper/classes";
+
 export class QuotePublicDto {
+	@AutoMap()
 	id!: string;
 
+	@AutoMap()
 	text!: string;
 
+	@AutoMap()
 	source?: string;
 
+	@AutoMap()
 	createdAt!: string;
 }
