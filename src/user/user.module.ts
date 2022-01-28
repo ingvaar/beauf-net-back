62a61b0f5174f45f07375f843eee2649ac71c17a
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2022-01-28 17:32:38 +0100

    feature/user-creation -> remove admin lock on user creation and add captcha validation

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ServicesModule } from '../services/services.module';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
@@ -8,6 +9,7 @@ import { UserService } from './user.service';
 @Module({
 	imports: [
 		TypeOrmModule.forFeature([UserEntity]),
+		ServicesModule,
 	],
 	controllers: [UserController],
 	providers: [UserService],
